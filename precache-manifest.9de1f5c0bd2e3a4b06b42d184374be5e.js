868946995f0b0202f82059dcf573bf6fa3005df7
Josh Wu <josh.wu@tradevan.com.tw>
2019-08-19 10:21:55 +0800

    Updates

---
+++ precache-manifest.9de1f5c0bd2e3a4b06b42d184374be5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6438fc381c968a05d30ffa3a4224207a",
+    "url": "/Week5_90sec_game/index.html"
+  },
+  {
+    "revision": "d5c7a3cc8a292a528ae2",
+    "url": "/Week5_90sec_game/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "e169b226a3aeae833180",
+    "url": "/Week5_90sec_game/static/js/2.e8138f8d.chunk.js"
+  },
+  {
+    "revision": "d5c7a3cc8a292a528ae2",
+    "url": "/Week5_90sec_game/static/js/main.f9ef50aa.chunk.js"
+  },
+  {
+    "revision": "f83b1654d9cdd535e8cc",
+    "url": "/Week5_90sec_game/static/js/runtime~main.8fc46bb4.js"
+  }
+]);
